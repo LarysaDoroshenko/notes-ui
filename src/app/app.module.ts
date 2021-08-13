1d8c5898eece6e3c0702ed04d3f32e3bbc4e8b89
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2021-08-14 01:23:58 +0300

    chore: rename a package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {NoteContainerComponent} from './container/notes/note-container.component';
+import {NoteContainerComponent} from './container/note/note-container.component';
 import {NoteService} from './service/note-service';
 import {NoteListComponent} from './component/note-list/note-list.component';
 import {NoteListItemComponent} from './component/note-list-item/note-list-item.component';
