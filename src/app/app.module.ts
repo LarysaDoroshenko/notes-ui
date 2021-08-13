f81cf2f61e35510ed9f88b0e7b34eb91709e2e5b
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2021-08-13 17:50:30 +0300

    chore: add a note-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {NoteContainerComponent} from './container/notes/note-container.component';
 import {NoteService} from './service/note-service';
+import {NoteListComponent} from './component/note-list/note-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NoteContainerComponent
+    NoteContainerComponent,
+    NoteListComponent
   ],
   imports: [
     BrowserModule,
